d2410c7b8168574885c0d1e9b47b621c62495cd5
Felipe Steffan <felip.developer@hotmail.com>
2022-11-03 12:05:05 -0300

    Inserindo Paginação

--- src/main/java/com/program/parking/controllers/ParkingController.java
+++ src/main/java/com/program/parking/controllers/ParkingController.java
@@ -8,6 +8,10 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -59,9 +63,9 @@ public class ParkingController {
 	}	
 	
 	@GetMapping
-	public ResponseEntity<List<ParkingSpotModel>> findAll() {
-		List<ParkingSpotModel> list = parkingService.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<ParkingSpotModel>> findAll(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+		Page<ParkingSpotModel> page = parkingService.findAll(pageable);
+		return ResponseEntity.ok().body(page);
    }	
 	
 	@GetMapping(value="/{id}")
--- src/main/java/com/program/parking/services/ParkingService.java
+++ src/main/java/com/program/parking/services/ParkingService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.program.parking.models.ParkingSpotModel;
@@ -40,8 +42,8 @@ public class ParkingService {
 		return parkingRepository.existsByApartmentAndBlock(apartment, block);
 	}
 	
-	public List<ParkingSpotModel> findAll() {
-		return parkingRepository.findAll();
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
+		return parkingRepository.findAll(pageable);
 	}	
 	
 	
